0f24a501a9dcfb7848b3466988bf8084bbc9bb34
wohu <hugo2004esperanza@gmail.com>
2024-01-23 09:26:28 +0100

    web con node y variables de entorno

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
+require("dotenv").config()
+
 const express = require("express");
 
 const app = express();
 
+const PORT = process.env.PORT || 5000
+
 app.get("/", (req, res) => {
 
-    res.send("<h1>Hola Mundo</h1>");
+    res.send("<h1>Hola Mundo</h1><p>");
     
 });
 
-app.listen(3000);
+//console.log(process.env.ENVIOR); //Si tenemos el .env, nos da ese valor del fichero
 
-console.log("Servidor en puerto 3000");
+app.get("/env", (req, res) => {
+
+    const t2 = "<h1>" + process.env.MYSQL_HOST + "</h1>"
+    const t1 = "<h1>" + process.env.MYSQL_PASS + "</h1>"
+    const t3 = "<h1>" + process.env.MYSQL_USER + "</h1>"
+
+    res.send(t1 + t2 + t3);
+
+    
+});
+
+app.get("/about", (req, res) => {
+
+    res.send("<h1>Acerca de...</h1>");
+    
+});
+
+
+app.listen(PORT);
+
+console.log("Servidor en puerto", PORT);
